521770a1a0b001a9ddc73bd650a7d1b961781a48
Nathan Birrell <nathanbirrell@gmail.com>
2014-07-19 15:48:27 +1000

    Navigation complete (for now)

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,11 +1,9 @@
-<nav class="top-bar" data-topbar>
-  <section class="top-bar-section">
-    <div class="centered-nav">
+<section class="top-bar-section">
+  <nav data-topbar>
       <ul>
         <?php foreach($pages->visible() AS $p): ?>
         <li><a <?php echo ($p->isOpen()) ? ' class="active"' : '' ?> href="<?php echo $p->url() ?>"><?php echo html($p->title()) ?></a></li>
         <?php endforeach ?>
       </ul>
-    </div>
-  </section>
-</nav>
+  </nav>
+</section>
