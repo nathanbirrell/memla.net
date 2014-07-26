47d1a1a7acdd61e80cbd17fb08e597d9fe3856c8
Nathan Birrell <nathanbirrell@gmail.com>
2014-07-26 12:37:05 +1000

    Started contact page

---
+++ site/templates/contact.php
@@ -0,0 +1,21 @@
+<?php snippet('header') ?>
+
+<section class="content">
+
+  <div class="row page-body contact people">
+      <div class="small-10 columns">
+        <h1><?php echo $page->people_title() ?></h1>
+
+        // TODO: for each person, echo their details
+      </div>
+  </div>
+
+  <div class="row page-body contact find-us">
+      <div class="small-10 columns">
+        <h1><?php echo $page->find_title() ?></h1>
+      </div>
+  </div>
+
+</section>
+
+<?php snippet('footer') ?>
