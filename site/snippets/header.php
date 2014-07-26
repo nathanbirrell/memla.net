80c00e8d00c0fc2400a9a3b62206781e6559cdc8
Nathan Birrell <nathanbirrell@gmail.com>
2014-07-26 11:11:28 +1000

    Added fontawesome

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -11,10 +11,6 @@
     <meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
     <meta name="robots" content="index, follow" />
 
-    <!-- If you are using the CSS version, only link these 2 files, you may add app.css to use for your overrides if you like -->
-    <link rel="stylesheet" href="assets/css/normalize.min.css">
-
-    <!-- If you are using the gem version, you need this only -->
     <link rel="stylesheet" href="assets/css/app.css">
 
     <script src="assets/js/vendor/modernizr.js"></script>
--- site/templates/about.php
+++ site/templates/about.php
@@ -5,6 +5,7 @@
   <div class="row page-body">
     <div class="large-12 columns about">
       <article class="col">
+        <i class="fa fa-camera-retro fa-5x"></i>
         <?php echo kirbytext($page->col_1()) ?>
       </article>
       <article class="col">
