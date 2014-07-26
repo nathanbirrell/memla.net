4eebaea9527b3366252f95968925291ba1f28109
Nathan Birrell <nathanbirrell@gmail.com>
2014-07-26 11:49:11 +1000

    Homepage complete

--- site/templates/about.php
+++ site/templates/about.php
@@ -2,19 +2,19 @@
 
 <section class="content">
 
-  <div class="row page-body">
-    <div class="large-12 columns about">
-      <article class="col">
-        <i class="fa fa-camera-retro fa-5x"></i>
+  <div class="row page-body about">
+      <article class="medium-4 columns col">
+        <i class="fa fa-users fa-5x"></i>
         <?php echo kirbytext($page->col_1()) ?>
       </article>
-      <article class="col">
-        <?php echo kirbytext($page->col_1()) ?>
+      <article class="medium-4 columns col">
+        <i class="fa fa-lightbulb-o fa-5x"></i>
+        <?php echo kirbytext($page->col_2()) ?>
       </article>
-      <article class="col">
-        <?php echo kirbytext($page->col_1()) ?>
+      <article class="medium-4 columns col">
+        <i class="fa fa-gavel fa-5x"></i>
+        <?php echo kirbytext($page->col_3()) ?>
       </article>
-    </div>
   </div>
 
 </section>
