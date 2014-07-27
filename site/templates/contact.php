d2c728b0d05a0605a8e8f1fb0a4b9d96911ea385
Nathan Birrell <nathanbirrell@gmail.com>
2014-07-27 14:45:46 +1000

    Set up grid foundation for "people" section

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -1,19 +1,24 @@
 <?php snippet('header') ?>
 
-<section class="content">
+<section class="content contact">
 
-  <div class="row page-body contact people">
-      <div class="small-10 columns">
-        <h1><?php echo $page->people_title() ?></h1>
-
-        // TODO: for each person, echo their details
-        // refer to http://getkirby.com/blog/one-pager for more help!!
+  <div class="row page-body people">
+    <h1><?php echo $page->people_title() ?></h1>
+      <div class="medium-8 medium-offset-2 columns">
+        <?php foreach($page->children() as $person): ?>
+          <person class="medium-4 medium-offset-2 columns">
+            <?php echo $person->name() ?>
+            <?php echo $person->phone() ?>
+            <?php echo $person->email() ?>
+          </person>
+        <?php endforeach ?>
       </div>
   </div>
 
-  <div class="row page-body contact find-us">
+  <div class="row page-body find-us">
+    <h1><?php echo $page->find_title() ?></h1>
       <div class="small-10 columns">
-        <h1><?php echo $page->find_title() ?></h1>
+
       </div>
   </div>
 
