0727f16c7ee55fd99d0687dfdf9d861687972d75
Nathan Birrell <nathanbirrell@gmail.com>
2014-10-30 11:55:11 +1100

    added link to image on projects page

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -33,7 +33,9 @@ function trimDescription($string,$length=220,$append="&hellip;") {
         <project class="medium-4 medium-offset-2 project-summary">
           
           <div class="project-image imageFill clearfix">
-            <img src="<?php echo $project_image->url() ?>">
+            <a href="<?php echo $project->url() ?>">
+              <img src="<?php echo $project_image->url() ?>">
+            </a>
           </div>
 
           <h2 class="project-title"><a href="<?php echo $project->url() ?>"><strong><?php echo $project->title() ?></strong></a></h2>
