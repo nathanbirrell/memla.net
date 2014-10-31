b15c36f265da4ae60179a40ee4bbc245612c08b8
Nathan Birrell <nathanbirrell@gmail.com>
2014-10-31 12:43:47 +1100

    Mobile styles complete

--- site/templates/project.php
+++ site/templates/project.php
@@ -1,15 +1,15 @@
 <?php snippet('header') ?>
 
+<div class="clearfix spacer"></div>
+
 <section class="content project">
 
-	<div class="gallery-controllers">
-		<span class="prevslide fa fa-angle-left" style="cursor: pointer;"></span>
-		<span class="nextslide fa fa-angle-right" style="cursor: pointer;"></span>
-	</div>
-
-
 	<div class="row section project-info">
-	    <div class="medium-12 columns">
+		<div class="gallery-controllers">
+			<span class="prevslide fa fa-angle-left" style="cursor: pointer;"></span>
+			<span class="nextslide fa fa-angle-right" style="cursor: pointer;"></span>
+		</div>
+	    <div class="small-10 small-centered columns">
 			<article class="info">
 				<h1 class="project-title"><?php echo html($page->title()) ?></h1>
 				
