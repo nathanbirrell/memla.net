5be5ec1b53126c77910178b5ce36144f8bdf8054
Nathan Birrell <nathanbirrell@gmail.com>
2015-05-04 11:56:50 +1000

    Tag field added to CMS

--- site/panel/blueprints/project.php
+++ site/panel/blueprints/project.php
@@ -7,6 +7,9 @@ fields:
   title: 
     label: Title
     type:  text
+  tags:
+    label: Tags
+    type: text
   text: 
     label: Description (recommended limit of 300 characters to look best on all devices.)
     type:  textarea
