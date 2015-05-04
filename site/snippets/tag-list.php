20c9c54d37b086d7a77beb593cf60d6e14de0fe1
Nathan Birrell <nathanbirrell@gmail.com>
2015-05-04 16:51:58 +1000

    added styles for a pretty tag hover state

--- site/snippets/tag-list.php
+++ site/snippets/tag-list.php
@@ -1,14 +1,16 @@
-<ul class="tags small-block-grid-1 medium-block-grid-3">
-    <?php foreach($tags as $tag): ?>
-    <li>
-      <div class="tag-image imageFill">
-      	<a href="<?php echo $page->url() . '/tag:' . $tag ?>">
-	      	<img src="<?= getTagThumbnail($tag, $page) ?>">
-      	</a>
-      </div>
-      <a href="<?php echo $page->url() . '/tag:' . $tag ?>">
-        <?php echo html(displayTag($tag)) ?>
-      </a>
-    </li>
-    <?php endforeach ?>
+<ul class="tags small-block-grid-1 medium-block-grid-2">
+	<?php foreach($tags as $tag): ?>
+		<li>
+			<div class="tag-image imageFill">
+				<a href="<?php echo $page->url() . '/tag:' . $tag ?>">
+					<img src="<?= getTagThumbnail($tag, $page) ?>">
+					<div class="tag-text hover">
+						<p><?php echo html(displayTag($tag)) ?> <br />
+						<i class="fa fa-chevron-circle-right"></i></p>
+					</div>
+				</a>
+			</div>
+
+		</li>
+	<?php endforeach ?>
 </ul>
