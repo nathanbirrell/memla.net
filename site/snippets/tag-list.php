92da134e408f3c3656db36f1ddd49785904e88a7
Nathan Birrell <nathanbirrell@gmail.com>
2015-05-04 17:47:28 +1000

    added animations to hover state of projects page

--- site/snippets/tag-list.php
+++ site/snippets/tag-list.php
@@ -1,16 +1,16 @@
-<ul class="tags small-block-grid-1 medium-block-grid-2">
+<ul class="tags small-block-grid-1 medium-block-grid-1 large-block-grid-2">
 	<?php foreach($tags as $tag): ?>
 		<li>
-			<div class="tag-image imageFill">
-				<a href="<?php echo $page->url() . '/tag:' . $tag ?>">
+			<a href="<?php echo $page->url() . '/tag:' . $tag ?>">
+				<div class="tag-image imageFill">
 					<img src="<?= getTagThumbnail($tag, $page) ?>">
-					<div class="tag-text hover">
+					<div class="tag-bg hide"></div>
+					<div class="tag-text">
 						<p><?php echo html(displayTag($tag)) ?> <br />
 						<i class="fa fa-chevron-circle-right"></i></p>
 					</div>
-				</a>
-			</div>
-
+				</div>
+			</a>
 		</li>
 	<?php endforeach ?>
 </ul>
