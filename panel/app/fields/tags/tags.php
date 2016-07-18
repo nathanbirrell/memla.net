0a1a23cfa0dcebfbdd3e77790164e1c7aa7c441d
Nathan Birrell <nathanbirrell@gmail.com>
2016-07-18 19:49:59 +1000

    update to kirby 2.3.1

--- panel/app/fields/tags/tags.php
+++ panel/app/fields/tags/tags.php
@@ -2,12 +2,6 @@
 
 class TagsField extends TextField {
 
-  static public $assets = array(
-    'js' => array(
-      'tags.min.js'
-    )
-  );
-
   public function __construct() {
 
     $this->icon      = 'tag';
@@ -24,7 +18,7 @@ class TagsField extends TextField {
     $input->addClass('input-with-tags');
     $input->data(array(
       'field'     => 'tags',
-      'lowercase' => $this->lower,
+      'lowercase' => $this->lower ? 'true' : false,
       'separator' => $this->separator,
     ));
 
@@ -34,13 +28,17 @@ class TagsField extends TextField {
 
     } else if($page = $this->page()) {
 
-      empty($this->field) ? $field = $this->name() : $field = $this->field;
+      $field = empty($this->field) ? $this->name() : $this->field;
+      $model = is_a($this->model, 'File') ? 'file' : 'page';
 
       $query = array(
         'uri'       => $page->id(),
         'index'     => $this->index(),
         'field'     => $field,
-        'separator' => $this->separator()
+        'yaml'      => $this->parentField,
+        'model'     => $model,
+        'separator' => $this->separator(),
+        '_csrf'     => panel()->csrf(),
       );
 
       $input->data('url', panel()->urls()->api() . '/autocomplete/field?' . http_build_query($query));
